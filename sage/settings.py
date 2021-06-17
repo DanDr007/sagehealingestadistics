c97b972813c734cdc86add2333c21754b77504e5
DanDr007 <danddr007@gmail.com>
2021-06-17 02:14:46 -0500

    D:

--- sage/settings.py
+++ sage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z73g$iz$w-u(l-#lv56vou&)w&$4*d!hn*bl2n(o)^fze*ch_m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
